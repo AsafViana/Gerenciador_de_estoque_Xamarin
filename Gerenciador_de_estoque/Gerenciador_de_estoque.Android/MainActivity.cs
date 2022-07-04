1cc6d861552a7ae98aca399fe8d1abaeb2583b42
AsafViana <asaff.viana@outlook.com>
2022-07-04 20:06:36 -0300

    a

--- Gerenciador_de_estoque/Gerenciador_de_estoque.Android/MainActivity.cs
+++ Gerenciador_de_estoque/Gerenciador_de_estoque.Android/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.Fingerprint;
+using Plugin.CurrentActivity;
 
 namespace Gerenciador_de_estoque.Droid
 {
@@ -19,6 +21,10 @@ namespace Gerenciador_de_estoque.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            CrossFingerprint.SetCurrentActivityResolver(() => CrossCurrentActivity.Current.Activity);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
